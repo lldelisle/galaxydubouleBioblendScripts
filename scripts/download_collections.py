fb31d3665870fb01e192f652230697d4dd10b2cc
Lucille Delisle <lucille.delisle@epfl.ch>
2021-03-23 16:16:27 +0100

    fix error with header + taking care of collection of pairs

--- scripts/download_collections.py
+++ scripts/download_collections.py
@@ -7,18 +7,34 @@ import argparse
 from bioblend.galaxy import GalaxyInstance
 
 
+def download_element(gi, e, current_output_folder):
+    identifier = e['element_identifier']
+    # If simple collection:
+    dataset_id = e['object']['id']
+    dataset_ext = e['object']['file_ext']
+    if not dataset_ext or dataset_ext == 'auto' or dataset_ext == '_sniff_':
+        dataset_ext = 'data'
+    gi.datasets.download_dataset(dataset_id,
+                                 file_path=os.path.join(current_output_folder, f"{identifier}.{dataset_ext}"),
+                                 use_default_filename=False)
+
 def download_collections(gi, collection_table, output_folder):
     if not os.path.isdir(output_folder):
         os.mkdir(output_folder)
     with open(collection_table, 'r') as f:
         for i, line in enumerate(f, 1):
             ls = line.split()
-            assert len(ls) >= 2, "collection table require 2 columns"
+            if len(ls) < 2:
+                print(f"Skipping line {i}")
+                continue
             potential_collection = line.split()[0]
             potential_history = line.split()[1]
-            infos = gi.histories.show_dataset_collection(potential_history, potential_collection)
+            try:
+                infos = gi.histories.show_dataset_collection(potential_history, potential_collection)
+            except Exception:
+                infos = None
             if not isinstance(infos, dict):
-                if i != 1 or potential_dataset != 'dataset_id':
+                if i != 1 or potential_collection != 'collection_id':
                     print(f"Error line {i}:")
                     print(infos)
                     print("Line ignored.")
@@ -28,21 +44,23 @@ def download_collections(gi, collection_table, output_folder):
                 collection_hid = infos['hid']
                 current_output_folder = os.path.join(output_folder,
                                                      infos['history_id'] + "_" + history_name.replace(' ', '_'),
-                                                     collection_hid + "_" + collection_name.replace(' ', '_'))
+                                                     str(collection_hid) + "_" + collection_name.replace(' ', '_'))
                 if not os.path.isdir(current_output_folder):
                     os.makedirs(current_output_folder)
                 print(f"Downloading {collection_name} in {current_output_folder}")
                 for e in infos['elements']:
-                    identifier = e['element_identifier']
-                    # If simple collection:
-                    dataset_id = e['object']['id']
-                    dataset_ext = e['object']['file_ext']
-                    if not dataset_ext or dataset_ext == 'auto' or dataset_ext == '_sniff_':
-                        dataset_ext = 'data'
-                    gi.datasets.download_dataset(dataset_id,
-                                                 file_path=os.path.join(current_output_folder, f"{identifier}.{dataset_ext}"),
-                                                 use_default_filename=False)
-
+                    if e['element_type'] == 'hda':
+                        # This was a simple collection
+                        print(f"Downloading {e['element_identifier']}")
+                        download_element(gi, e, current_output_folder)
+                    else:
+                        # This was a collection of pairs:
+                        new_output_folder = os.path.join(current_output_folder, e['element_identifier'])
+                        if not os.path.isdir(new_output_folder):
+                            os.makedirs(new_output_folder)
+                        for ee in e['object']['elements']:
+                            print(f"Downloading {ee['element_identifier']}")
+                            download_element(gi, ee, new_output_folder)
 
 def parse_arguments(args=None):
     argp = argparse.ArgumentParser(
